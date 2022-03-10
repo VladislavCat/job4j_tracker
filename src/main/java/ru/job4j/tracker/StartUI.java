c731b6ba29d7ee02c3ea65ee1fb1747e6e0d40e9
VladislavCat <kshift84@gmail.com>
2022-03-11 01:05:55 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,46 @@
 package ru.job4j.tracker;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
 
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            snowMenu();
+            System.out.println("Select: ");
+            int select;
+            try {
+                select = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Введите именно число! ");
+                continue;
+            }
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    public void snowMenu() {
+        String[] menu = {
+                "Add Item", "Snow All items",
+                "Edit item", "Delete item", "Find item by id",
+                "Find items by name", "Exit program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
-        Item item = new Item("Заявка", 1212);
-        System.out.println(item);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
