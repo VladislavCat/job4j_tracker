10ccd439d7b33696a7a74b76b1d7dcf2c905c3ec
VladislavCat <kshift84@gmail.com>
2022-03-12 01:05:23 +0300

    2.8. Реализация класса StartUI. Вывод заявок по имени [#500749]

---
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Автобус едет без пассажиров");
+    }
+
+    @Override
+    public void driveWithPass(int numOfPass) {
+        System.out.println("Автобус едет с таким кол-вом пассажиров" + numOfPass);
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return fuel * 44;
+    }
+}
