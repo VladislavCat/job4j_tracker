9633314f5ea02e31194bb644b5913ec19550c0ca
VladislavCat <kshift84@gmail.com>
2022-03-14 15:11:19 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,21 +1,15 @@
 package ru.job4j.tracker;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class StartUI {
 
-    public void init(Scanner scanner, Tracker tracker) throws InterruptedException {
+    public void init(Input input, Tracker tracker) throws InterruptedException {
         boolean run = true;
         while (run) {
             snowMenu();
-            System.out.println("Select: ");
-            int select;
-            select = Integer.parseInt(scanner.nextLine());
+            int select = input.askInt("Select: ");
             if (select == 0) {
                 System.out.println("---Create item---");
-                System.out.println("Enter name: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Заявка добавлена: " + item);
@@ -31,10 +25,8 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("--- Edit item ---");
-                System.out.println("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                int id = input.askInt("Enter id: ");
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
                     System.out.println("Заявка заменена");
@@ -43,8 +35,7 @@ public class StartUI {
                 }
             } else if (select == 3) {
                 System.out.println("--- Delete item ---");
-                System.out.print("Enter id item: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id item: ");
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена.");
                 } else {
@@ -52,8 +43,7 @@ public class StartUI {
                 }
             } else if (select == 4) {
                 System.out.println("--- Find item by id ---");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id: ");
                 Item item = tracker.findById(id);
                 if (item != null) {
                     System.out.println(item);
@@ -64,7 +54,7 @@ public class StartUI {
             } else if (select == 5) {
                 System.out.println("--- Find items by name ---");
                 System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item[] items = tracker.findByName(name);
                 if (items.length > 0) {
                     for (Item item : items) {
@@ -95,10 +85,10 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         try {
-            new StartUI().init(scanner, tracker);
+            new StartUI().init(input, tracker);
         } catch (InterruptedException e) {
             System.out.println("Поток досрочно завершился, ибо я криворукий");
         }
