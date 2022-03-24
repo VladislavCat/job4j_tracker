06ced5fe18516f5b3f7b52da6b20905fb62c4b25
VladislavCat <kshift84@gmail.com>
2022-03-24 12:02:14 +0300

    Добавление теста

--- src/test/java/learn/collection/OrderConvertTest.java
+++ src/test/java/learn/collection/OrderConvertTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,5 +18,17 @@ public class OrderConvertTest {
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
         Assert.assertEquals(map.get("3sfe"), new Order("3sfe", "Dress"));
+    }
+
+    @Test
+    public void whenListFourConvertToHashThree() {
+        List<Order> orders = Arrays.asList(
+                new Order("3sfe", "Dress"),
+                new Order("12Np", "Alt"),
+                new Order("1234Jue", "End"),
+                new Order("12Np", "Duplicate")
+        );
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        Assert.assertEquals(map.size(), 3);
     }
 }
