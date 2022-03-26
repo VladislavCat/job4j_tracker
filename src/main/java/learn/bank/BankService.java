817c003ca4bdc40ad38fc2f3d006c8baaba2b385
VladislavCat <kshift84@gmail.com>
2022-03-26 15:47:29 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/learn/bank/BankService.java
+++ src/main/java/learn/bank/BankService.java
@@ -3,12 +3,25 @@ package learn.bank;
 import java.util.*;
 
 public class BankService {
+    /**
+     * Поле класса содержащее в себе пользователя и его счета
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод добавляет нового пользователя банка
+     * @param user пользователь, которого добавят в систему
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод проверяет, что по такому паспорту в банке есть пользователь, а затем добавляет
+     * ему новый счет в список его счетов
+     * @param passport паспорт пользователя, по которому происходит поиск
+     * @param account аккаунт, который добавится к списку
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -19,6 +32,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод ищет, что в поле объекта есть пользователь с таким паспортом
+     * @param passport по которому пользователь ищется в поле
+     * @return если введен верно, вернет найденного пользователя
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
@@ -28,6 +46,13 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод проверяет, что по паспорту есть пользователь, а затем ищет по реквизитам нужный аккаунт
+     * среди списка аккаунтов пользователя
+     * @param passport принимает паспорт, по которому ищет список аккаунтов
+     * @param requisite по которым ищется нужный аккаунт
+     * @return если данные верны, метод вернет нужный аккаунт
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -41,6 +66,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод проверяет, что по входным данным существуют счета, а затем совершает перевод с одного счета на другой.
+     * @param srcPassport паспорт отправителя, по которым ищется аккаунт
+     * @param srcRequisite реквизиты отправителя, по которым ищется счет
+     * @param destPassport паспорт получателя, по которым ищется аккаунт
+     * @param destRequisite реквизиты получателя, по которым ищется счет
+     * @param amount сумма перевода
+     * @return возвращает значение, в зависимости от того, был совершен перевод или нет
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite,
                                  double amount) {
