082206703709a5d2fff14f5dd8ab87bf952fa2e2
VladislavCat <kshift84@gmail.com>
2022-04-04 01:04:16 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/learn/stream/Profiles.java
@@ -0,0 +1,14 @@
+package learn.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+}
