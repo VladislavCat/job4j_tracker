c2cf1cd1d53dc57ebf7c7e6700de1f4bfb895c99
VladislavCat <kshift84@gmail.com>
2022-04-04 12:20:59 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/learn/stream/Profiles.java
+++ src/main/java/learn/stream/Profiles.java
@@ -1,6 +1,5 @@
 package learn.stream;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +15,7 @@ public class Profiles {
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted(new CompAddressToCity())
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .toList();
     }
