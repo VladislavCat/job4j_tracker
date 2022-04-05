e004f7ef651cddcecab5f5e57ab719dd3a43d57f
VladislavCat <kshift84@gmail.com>
2022-04-05 12:27:10 +0300

    Исправление

--- src/main/java/learn/stream/Analyze.java
+++ src/main/java/learn/stream/Analyze.java
@@ -31,7 +31,7 @@ public class Analyze {
         return stream
                 .flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors
-                        .groupingBy(Subject::getName,
+                        .groupingBy(Subject::getName, LinkedHashMap::new,
                                 Collectors.averagingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(tuple -> new Tuple(tuple.getKey(), tuple.getValue()))
@@ -54,7 +54,7 @@ public class Analyze {
         return stream
                 .flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors
-                        .groupingBy(Subject::getName, LinkedHashMap::new,
+                        .groupingBy(Subject::getName,
                                 Collectors.summingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(tuple -> new Tuple(tuple.getKey(), tuple.getValue()))
